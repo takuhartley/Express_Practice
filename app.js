7ccc2851c14df25abafe741e373ec5aa1e7762a4
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-12 08:42:44 +0900

    adding navigational flexibility

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Sup bitches.'))
 app.get('/random', (req, res) => {res.send([1,2,3])})
-app.get('/name/:id', (req, res) => res.send(id))
+app.get('/name/:id', (req, res) => res.send(req.params.id))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}!`))
