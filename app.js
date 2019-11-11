07396cb1b213a67faea79e6d279d6db8f68cb8da
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-12 08:37:02 +0900

    adding port settings

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Sup bitches.'))
+app.get('/random', (req, res) => {res.send([1,2,3])})
+app.get('/name/:id', (req, res) => res.send(id))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}!`))
