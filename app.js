38808a7acf27ad3a1043f88b9dd566b544254cc9
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-12 09:17:17 +0900

    post working with postman

--- app.js
+++ app.js
@@ -7,22 +7,19 @@ const cars = [
 ];
 app.use(express.json());
 app.get('/', (req, res) => res.send(cars));
-app.get('/random', (req, res) => {
-    res.send([1, 2, 3]);
-});
 app.get('/name/:id', (req, res) => res.send(req.params.id));
 app.get('/cars/:id', (req, res) => {
     const coolcars = cars.find(c => c.id === parseInt(req.params.id));
     if (!coolcars) res.status(404).send('The car with the given id was not found');
     res.send(coolcars);
 })
-app.post('/cars', (req, res) => {
-    const cars = {
-        id: car.length + 1,
+app.post('/newcar', (req, res) => {
+    const car = {
+        id: cars.length + 1,
         name: req.body.name
     };
-    cars.push(cars);
-    res.send(cars);
-})
+    cars.push(car);
+    res.send(car);
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
